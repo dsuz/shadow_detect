9c2354cc28e3a957ca03a41be96a3d1db22245d7
Daisuke Suzuki <bboydaisuke@gmail.com>
2017-12-14 17:38:02 +0900

    Initial commit

---
+++ Assets/ShadowUtility.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShadowUtility : MonoBehaviour
+{
+    public Text m_messsageText;
+
+    public void OnEnterShadow()
+    {
+        if (m_messsageText) m_messsageText.text = "影を踏んでいる";
+    }
+
+    public void OnExitShadow()
+    {
+        if (m_messsageText) m_messsageText.text = "";
+    }
+}
